2614ea8fbb4809eff9087e2f0bd9501f1cb7c7f7
scott0929 <scott0929@gmail.com>
2024-11-27 15:42:41 -0800

    sds

--- a.py
+++ a.py
@@ -1,17 +1,37 @@
 import os
 import sys
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAI
+from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 
-from langchain.document_loaders import TextLoader
-from langchain.indexes import VectorstoreIndexCreator
-
 load_dotenv()
-
-os.environ['OPENAI_API_KEY'] = constants.APIKEY
+os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
 
 query = sys.argv[1]
 
+# Load the document
 loader = TextLoader("first.txt")
-index = VectorstoreIndexCreator().from_loaders([loader])
+documents = loader.load()
 
-print(index.query(query))
+# Split document into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+split_documents = text_splitter.split_documents(documents)
+
+# Define embeddings and vectorstore
+embeddings = OpenAIEmbeddings()
+vectorstore = FAISS.from_documents(documents, embeddings)
+
+# Initialize the LLM
+llm = OpenAI(temperature=0)
+
+# Set up a Retrieval QA chain
+retriever = vectorstore.as_retriever()
+qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+
+# Query the index
+response = qa_chain.run(query)
+print(response)
